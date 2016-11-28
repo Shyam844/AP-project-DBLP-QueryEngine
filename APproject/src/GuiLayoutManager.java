284feb211d86603014ba24658fb9529be86354d7
divansh <divanshar@gmail.com>
2016-11-28 11:41:08 +0530

    Added GUI changed mainClass displayWindow

--- APproject/src/GuiLayoutManager.java
+++ APproject/src/GuiLayoutManager.java
@@ -1,42 +1,37 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.StringReader;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class GuiLayoutManager implements ActionListener {
 //Card layout
 	private JPanel cards;
+	private static int a=3;
 	public void addComponentsToPane(Container pane)
 	{
-		 	JPanel comboBoxPane = new JPanel(); //made a new panel to hold all the cards
-			final String QUERY1 = "Query1";
+		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+		 	JPanel comboBoxPane = new JPanel(); 
+		 	final String QUERY1 = "Query1";
 			final String QUERY2 = "Query2";
-			
-
 	        String comboBoxItems[] = { QUERY1, QUERY2}; // added first combobox items
 	        JComboBox<String> cb = new JComboBox(comboBoxItems);
-	        cb.setEditable(false);
-	        cb.addActionListener(this);
-	        comboBoxPane.add(cb);
-	        JPanel emptyCard = new JPanel();
-	        JPanel card2 = new JPanel();
-	        card2.add(new JTextField("TextField", 20));      
-	        //Create the panel that contains the "cards".
-	        cards = new JPanel(new CardLayout());
-	        cards.add(emptyCard);
-	        cards.add(createAndGetCard1(), QUERY1);
-	        cards.add(card2, QUERY2);
-	        
-	        pane.add(comboBoxPane, BorderLayout.PAGE_START);
-	        pane.add(cards, BorderLayout.CENTER);
+	        System.out.println(pane.getLayout().toString());
+	        pane.add(cb);
+	        pane.add(new JButton("dhsgcvj"));
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -80,7 +75,23 @@ public class GuiLayoutManager implements ActionListener {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Add a new card to cards===============");
+				JComboBox<String > jb  = (JComboBox<String>)e.getSource();
+				System.out.println(jb.getSelectedIndex());
+				if(jb.getSelectedIndex()==0)
+				{
+					card1.add(new JButton("0"));
+					card1.setLayout(new BoxLayout(card1, BoxLayout.PAGE_AXIS));
+					card1.revalidate();
+					System.out.println("add new jbutton");
+				}
+				else if(jb.getSelectedIndex()==1)
+				{
+					
+				}
 				
+				
+				//card1.add(createFormSubType1(searchByList[jb.getSelectedIndex()]));
+				//card1.add(createFormSubType1("Name"));
 			}
 		});
 		card1.add(searchByComboBox);
@@ -88,8 +99,8 @@ public class GuiLayoutManager implements ActionListener {
 	}
     public JPanel createAndGetCard2() {
     	
-    	JPanel card1 = new JPanel();
-    	String[] searchByList= {"By Name","By Author"};
+    	JPanel cardTemp = new JPanel();
+    	String[] searchByList= {"By Author","By Title"};
     	 JComboBox<String> searchByComboBox = new JComboBox<>(searchByList); 
     	 searchByComboBox.addActionListener(new ActionListener() {
 			
@@ -99,9 +110,26 @@ public class GuiLayoutManager implements ActionListener {
 				
 			}
 		});
-		card1.add(searchByComboBox);
-		return card1;
+		cardTemp.add(searchByComboBox);
+		return cardTemp;
 	}
+		
+    public JPanel createFormSubType1(String byX) {
+    	JPanel tPanel = new JPanel();
+    	tPanel.add(new JLabel(byX));//ADD CHECKS
+    	tPanel.add(new JLabel("Since Year"));
+    	tPanel.add(new JLabel("Custom Range Start"));
+    	tPanel.add(new JLabel("Custom Range End"));
+    	ButtonGroup bg1 = new ButtonGroup();
+    	bg1.add(new JRadioButton("Sort by year"));
+    	bg1.add(new JRadioButton("Sort by relevance"));
+    	
+
+    	
+    	
+    	return tPanel;
+    	
+    }
 
 
 }
